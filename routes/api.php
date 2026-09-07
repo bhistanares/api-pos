06b30aec3f12e63e3c16f8ad7209b733d4741501
bhistanares <nares.sugiyanto@gmail.com>
2026-09-07 13:00:09 +0700

    feat customer

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Api\V1\ProductCategoryController;
 use App\Http\Controllers\Api\V1\ProductCategoryImageController;
 use App\Http\Controllers\Api\V1\ProductController;
 use App\Http\Controllers\Api\V1\ProductImageController;
+use App\Http\Controllers\Api\V1\CustomerController;
 
 Route::prefix('v1')->group(function() {
     Route::post('/login', [AuthController::class, 'login']);
@@ -21,5 +22,8 @@ Route::prefix('v1')->group(function() {
 
         Route::post('products/{id}/image', [ProductImageController::class, 'store']);
         Route::apiResource('products', ProductController::class);
+        
+        Route::get('customers/options', [CustomerController::class, 'options']);
+        Route::apiResource('customers', CustomerController::class);
     });
 });
